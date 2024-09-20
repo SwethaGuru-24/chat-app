e1f95b0c9ab0fad902f73f993883970d88d43998
SwethaGuru-24 <swetha.g.ihub@snsgroups.com>
2024-09-20 18:18:47 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,22 +1,17 @@
 const socket = io();
 
-// Listen for messages from the server
 socket.on('message', message => {
     outputMessage(message);
 });
 
-// Handle message submit
 const chatForm = document.getElementById('chat-form');
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    // Get message text
     const msg = e.target.elements.msg.value;
 
-    // Emit message to server
     socket.emit('chatMessage', msg);
-
-    // Clear input
+  
     e.target.elements.msg.value = '';
     e.target.elements.msg.focus();
 });
